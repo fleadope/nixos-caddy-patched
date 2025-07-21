0d135296954227be06dadaca7d420acf1d3b2bf5
fleadope <leepope@gmail.com>
2025-07-21 15:30:59 -0700

    updated to latest version of caddy/go

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Patched version of Caddy";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs =
     { self, nixpkgs }:
@@ -44,7 +44,7 @@
             inherit version;
             src = ./caddy-src;
             runVend = true;
-            vendorHash = "sha256-c9A0LabGN8gIq2pL/WP8wst3y0+8f72K015QZrX9yq4=";
+            vendorHash = "sha256-gFNo9vMlzgC/SFcFcdtWzzVROBPsRlCJswcD3gWNkSw";
             # vendorHash = pkgs.lib.fakeHash;
 
             meta = {
